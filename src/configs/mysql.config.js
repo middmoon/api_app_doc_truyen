23f8333481f2188a30cfe4652a81b8b1c789e16b
Nguyê~n Minh Thành <middmoon.68@gmail.com>
2023-12-10 19:14:48 +0700

    update the loai tim kiem

--- src/configs/mysql.config.js
+++ src/configs/mysql.config.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "appdoctruyen",
+  // host: "localhost",
+  // user: "root",
+  // password: "1234",
+  // database: "appdoctruyen",
+
+  host: "103.200.23.139",
+  user: "middmoon_test1",
+  password: "2$}qoPq]3]Yg",
+  database: "middmoon_appdoctruyen",
 });
 
 module.exports = db;
--- src/controller/theloai.controller.js
+++ src/controller/theloai.controller.js
@@ -41,31 +41,44 @@ async function truyentheloai(TenTheLoai) {
       db.query(
         `SELECT 
           truyen.idTruyen,
-          truyen.Ten,
+          truyen.Ten AS TenTruyen,
           DATE_FORMAT(truyen.NamPhatHanh, '%d-%m-%Y') AS NamPhatHanh,
-          truyen.Anh,
-          truyen.Mota,
-          JSON_ARRAYAGG(
-            JSON_OBJECT(
-              'idTacGia', tacgia.idTacGia,
-              'Ten', tacgia.Ten,
-              'Anh', tacgia.Anh,
-              'MoTa', tacgia.MoTa,
-              'NamSinh', DATE_FORMAT(tacgia.NamSinh, '%d-%m-%Y') )) AS TacGia
-          FROM
-            truyen
-            INNER JOIN
+          truyen.Anh AS AnhTruyen,
+          truyen.Mota AS MotaTruyen,
+          JSON_ARRAYAGG(JSON_OBJECT('idTacGia',
+                          tacgia.idTacGia,
+                          'Ten',
+                          tacgia.Ten,
+                          'Anh',
+                          tacgia.Anh,
+                          'MoTa',
+                          tacgia.MoTa,
+                          'NamSinh',
+                          tacgia.NamSinh)) AS TacGia,
+          (SELECT 
+                  JSON_ARRAYAGG(JSON_OBJECT('idTheLoai',
+                                      theloai.idTheLoai,
+                                      'TenTheLoai',
+                                      theloai.TenTheLoai))
+              FROM
+                  theloai
+                      INNER JOIN
+                  theloai_truyen ON theloai_truyen.idTheLoai = theloai.idTheLoai
+              WHERE
+                  truyen.idTruyen = theloai_truyen.idTruyen) AS TheLoai
+            FROM
+                truyen
+                    INNER JOIN
                 theloai_truyen ON truyen.idTruyen = theloai_truyen.idTruyen
-            INNER JOIN
+                    INNER JOIN
                 theloai ON theloai.idTheLoai = theloai_truyen.idTheLoai
-            INNER JOIN
+                    INNER JOIN
                 tacgia_truyen ON truyen.idTruyen = tacgia_truyen.idTruyen
-            INNER JOIN
+                    INNER JOIN
                 tacgia ON tacgia.idTacGia = tacgia_truyen.idTacGia
             WHERE
                 theloai.TenTheLoai = '${TenTheLoai}'
-            GROUP BY 
-                truyen.idTruyen;`,
+            GROUP BY truyen.idTruyen;`,
         (error, results, fields) => {
           if (error) {
             reject({
@@ -172,6 +185,13 @@ class TheLoaiController {
               NamSinh: item2.NamSinh,
               Anh: anhTacGiaPath(item2.idTacGia, item2.Ten, item2.Anh),
               MoTa: item2.MoTa,
+            };
+          }),
+
+          TheLoai: JSON.parse(item.TheLoai).map((item3) => {
+            return {
+              idTheLoai: item3.idTheLoai,
+              TenTheLoai: item3.TenTheLoai,
             };
           }),
         };
